08f1f478d462e720611ddeacefaf599d43dc84a7
weamosama <weam.hassanein@gmail.com>
2023-09-07 13:07:23 -0700

    test

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+nclude <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: always 0
+ *     */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
