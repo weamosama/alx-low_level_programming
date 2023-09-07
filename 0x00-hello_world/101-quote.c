5b7e0515596126fc3641713def883ed661ac4ab5
weamosama <weam.hassanein@gmail.com>
2023-09-07 15:03:05 -0700

    r

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-*main - Entry point
-*Return: Always 1 (Success)
+ *main - Entry point
+ *Return: Always 1 (Success)
 **/
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, message, 80);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return(1);
 }
