1307267d7c168f5b07fd3d9c491b08133ffbc23e
weamosama <weam.hassanein@gmail.com>
2023-09-07 15:00:05 -0700

    r

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
 **/
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.\n";
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(STDERR_FILENO, message, 80);
 	return(1);
 }
