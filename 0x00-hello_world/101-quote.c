5482b3ac994aced1e6e60f1a74b00f93d2572bc0
weamosama <weam.hassanein@gmail.com>
2023-09-07 14:56:58 -0700

    r

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
 *main - Entry point
-*Return: Always 1 (Suc
-*cess)
+*Return: Always 1 (Success)
 **/
-int main(void) 
+int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar,");
-	fprintf(stderr, " 2015-10-19,\n");
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.\n";
+	write(STDERR_FILENO, message, 80);
 	return (1);
-}
 
