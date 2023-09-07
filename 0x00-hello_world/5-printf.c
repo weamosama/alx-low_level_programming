5001c352ac1ee24d618d5fca78be5a74a57a3bc8
weamosama <weam.hassanein@gmail.com>
2023-09-07 13:16:58 -0700

    test

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/*
- * main - Entry poin
- * 
- *
- * Return: always 0
-  */
+/**
+ *  * main - Prints "with proper grammar, but the outcome is
+ *                   a piece of art," followed by a new line.
+ *    
+ *      Return: Always 0.
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
 }
