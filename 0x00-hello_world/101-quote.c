c3cc1eb46fabc6837beb9bc9fea9e2f5b76f53de
weamosama <weam.hassanein@gmail.com>
2023-09-07 14:43:56 -0700

    r

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main - Entry point Return: Always 1 (Succ
- *ess)
+ *main - Entry point Return: Always 1 (Suc
+ *cess)
 **/
 int main(void) 
 {
 	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return 1;
+	return (1);
 }
 
