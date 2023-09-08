f0d7daac065512d695fca7029451496999ea9dcd
weamosama <weam.hassanein@gmail.com>
2023-09-08 07:54:30 -0700

    test

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,10 @@ int main(void)
 
 	n = rand() - RAND_MAX / 2;
 
-	if(n<0)
+	if ( n < 0)
 	{
 		printf ("%d is negative\n", n);
-	} else if(n==0)
+	} else if (n == 0)
 	{
 		printf ("%d is zero\n", n);
 	} else
