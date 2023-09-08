0a5d94aa2647565655f732759002a06dabd4b59f
weamosama <weam.hassanein@gmail.com>
2023-09-08 10:29:18 -0700

    test

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *main - entry
+ *Return:0
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for(k=j+1;k<10;k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (k < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
