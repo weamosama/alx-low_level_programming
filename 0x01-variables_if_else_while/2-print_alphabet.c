9cc3e9d56acabb84ed15e3308c4c392cb878f007
weamosama <weam.hassanein@gmail.com>
2023-09-08 08:53:29 -0700

    test

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - entry 
+ *Return:0
+*/
+int main()
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+											}
+	}
+	putchar('\n');
+	return (0);
+}
