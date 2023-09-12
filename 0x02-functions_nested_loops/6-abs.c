2a5ee09fed14be12f9c3add0faa3715cf74fc111
weamosama <weam.hassanein@gmail.com>
2023-09-12 16:17:03 -0700

    test

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - main function
+ *
+ * @n: number to get its abs value
+ * Return: int
+ *
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
