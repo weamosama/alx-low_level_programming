a2547dbbe5eb9a42cccec4041cc487db2f08a1df
weamosama <weam.hassanein@gmail.com>
2023-09-14 07:29:13 -0700

    m

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,18 @@ void print_square(int n)
 
 	int j;
 
-	for (i = n; i > 0; i--)
+	if (n <= 0)
 	{
-		for(j = n; j > 0; j--)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
+	} else
+	{
+		for (i = n; i > 0; i--)
+		{
+			for(j = n; j > 0; j--)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
