73b1ec5f9b190785fa7274273d17c5aee6359346
weamosama <weam.hassanein@gmail.com>
2023-09-14 06:45:26 -0700

    m

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers -print_numbers
+ * Return: void
+ */
+void print_numbers()
+{
+	int i;
+
+	for (i=0; i < 10; i++)
+		_putchar('0' + i);
+	_putchar('\n');
+}
