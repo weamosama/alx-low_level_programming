3fb9dc16bb0bbf5a5c4d919cd0aebf7017735392
weamosama <weam.hassanein@gmail.com>
2023-09-18 11:52:17 -0700

    g

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+#include"main.h";
+/**
+ * reset_to_98 - test
+ * @n: int
+ */
+void reset_to_98(int *n){
+	*n=98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+void reset_to_98(int *n);
+/**
+ * swap_int - swapts integer values
+ * @a: first integer
+ * @b: second integer
+ */
+void swap_int(int *a, int *b);
+/**
+ * _strlen - length of string
+ * @s: string
+ *
+ * Return: returns length
+ */
+int _strlen(char *s);
+#endif
