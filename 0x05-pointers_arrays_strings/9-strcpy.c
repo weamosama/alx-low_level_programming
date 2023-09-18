2a0c3a11aee5e028cc39d1dd1a06b1b614c5bf17
weamosama <weam.hassanein@gmail.com>
2023-09-18 14:39:02 -0700

    t

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -18,7 +18,7 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[i] = src[i];
 	}
-	 if (_strlen(dest) > _strlen(src))
-                dest[size]='\0';
+	if (_strlen(dest) > _strlen(src))
+		dest[size]='\0';
 	return (dest);
 }
