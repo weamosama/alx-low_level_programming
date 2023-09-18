c1b2eadfb3a342893870b855deee892e31d6418a
weamosama <weam.hassanein@gmail.com>
2023-09-18 13:55:24 -0700

    g

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,8 +11,8 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n ; i++)
 	{
-		printf("%d" , a[i]);
-		if (i != n-1)
+		printf("%d",a[i]);
+		if (i != n - 1)
 			printf(", ");
 	}
 		printf("\n");
