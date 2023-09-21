8fcc441ee8be1c39e73ba3aa7c4fee815295c163
weamosama <weam.hassanein@gmail.com>
2023-09-21 11:53:49 -0700

    f

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
  *_stract -test
  *@dest: r
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stdio.h>
 char *_strcat(char *dest, char *src);
 #endif
