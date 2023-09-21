cb64926fd13666d7050565d2161dea5c53ed6835
weamosama <weam.hassanein@gmail.com>
2023-09-21 11:42:26 -0700

    f

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _stract -test
+ * @dest: r
+ * @src: f
+ * Return: char
+ *
+ */
+char *_strcat(char *dest, char *src)
+{  
+       int i;
+	
+       int destSize=0;
+	
+       int srcSize;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		destSize++;
+	for (srcSize = 0; src[srcSize] != '\0'; srcSize++)
+		dest[destSize + srcSize] = src[srcSize];
+	dest[destSize + srcSize] = '\0';
+	return (dest);
+}
