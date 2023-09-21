046f1a9fb8257369044907f1fd1d5740234354a7
weamosama <weam.hassanein@gmail.com>
2023-09-21 13:00:04 -0700

    f

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,6 +12,8 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (j = 0; j < n; j++)
 	{
+		if (src[j] == '\0')
+			break;
 		dest[j] = src[j];
 	}
 	if (j < n-1)
@@ -20,6 +22,5 @@ char *_strncpy(char *dest, char *src, int n)
 			dest[j] = '\0';
 	}
 	dest[j] = '\0';
-
 	return (dest);
 }
