dac3d04dd9de34235c567b2a4237130d59d2937f
weamosama <weam.hassanein@gmail.com>
2023-09-21 14:09:03 -0700

    f

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ *cap_string - Capitalize all words in a string.
+ *@str: Pointer to the input string.
+ *Return: Pointer to the modified string.
+ */
+char *cap_string(char *str)
+{
+	if (str == NULL)
+		return NULL;
+
+	int i = 0;
+
+	int new_word = 1;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+			str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+			str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+			str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		{
+			new_word = 1;
+		}
+		else if (new_word)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+			new_word = 0;
+		}
+		else
+		{
+			new_word = 0;
+		}
+
+		i++;
+	}
+
+	return (str);
+}
