511a29b414917234e7e2eb8b03c965380c935d89
weamosama <weam.hassanein@gmail.com>
2023-09-21 12:48:28 -0700

    f

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncpy - Concatenate two strings.
+ * @dest: The destination string.
+ * @src: The source string to append.
+ * @n: test
+ * Return: A pointer to the concatenated string (dest).
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		if (src[j] == '\0')
+			break;
+		dest[j] = src[j];
+	}
+
+	dest[j] = '\0';
+
+	return (dest);
+}
