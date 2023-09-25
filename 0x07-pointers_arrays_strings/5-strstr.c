3133fe7a9673779cb5fd70885b60d3863792bed8
weamosama <weam.hassanein@gmail.com>
2023-09-25 15:14:48 -0700

    d

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,19 +8,22 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int i, j, found = 1;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		found = 1;
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (s[i] == accept[j])
+			if (haystack[i] != noodle[j])
 			{
-				return (s + i);
+				found = 0;
+				break;
 			}
+			
 		}
+		if (found == 1)
+			return (&haystack[i])
 	}
-	if (s[i] == accept[j])
-		return (s + i);
 	return (Null);
 }
