1b7adc5f20849dc179818f661d3e60644528599e
weamosama <weam.hassanein@gmail.com>
2023-09-25 14:48:56 -0700

    d

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
-		{	
+		{
 			if (s[i] == accept[j])
 			{
 				count++;
@@ -22,7 +22,6 @@ unsigned int _strspn(char *s, char *accept)
 		}
 		if (s[i] != accept[j])
 			break;
-	}
-	
+	}	
 	return (count);
 }
