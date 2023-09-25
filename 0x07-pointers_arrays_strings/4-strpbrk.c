c3754a52803af46ef294e0a25aae2590acfc445c
weamosama <weam.hassanein@gmail.com>
2023-09-25 14:55:20 -0700

    d

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strpbrk - test
+ * @s: char
+ * @accept: char
+ * Return: int
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	if (s[i] == accept[j])
+		return (s + i);
+	return (Null);
+}
