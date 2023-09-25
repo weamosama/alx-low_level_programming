40d3ab4bd60586bec9b24deceb98281d717d932d
weamosama <weam.hassanein@gmail.com>
2023-09-25 14:32:20 -0700

    d

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - test
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
