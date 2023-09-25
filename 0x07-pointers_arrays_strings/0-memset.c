3948499ccc0b09c4234496d537f59f8846bf3f31
weamosama <weam.hassanein@gmail.com>
2023-09-25 14:25:17 -0700

    d

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,13 +3,14 @@
  * _memset - test
  * @s: char
  * @b: char
+ * @n: int
  * Return: char
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
 
-	for (i = n; i >= 0;i--)
+	for (i = n; i >= 0; i--)
 	{
 		s[i] = b;
 	}
