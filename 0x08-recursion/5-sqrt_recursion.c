1d262c576ad0720039dd3356f1a83ac21f6bf34d
weamosama <weam.hassanein@gmail.com>
2023-09-28 09:40:26 -0700

    ggg

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,13 +11,13 @@ int _sqrt_recursion(int n)
 		return (-1);
 	if (n == 0 || n == 1)
 		return (n);
-	return helper_sqrt(n, 2);
+	return (helper_sqrt(n, 2));
 }
 
 /**
  *helper_sqrt - a recursive helper
  *@n: the number to calculate the square root of
- *@i: the current guess for the square root
+ *@j: the current guess for the square root
  *
  *Return: the square root of n, or -1 if n does not have a natural square root
  */
@@ -27,5 +27,5 @@ int helper_sqrt(int n, int j)
 		return (j);
 	if (j * j > n)
 		return (-1);
-	return helper_sqrt(n, i + 1);
+	return (helper_sqrt(n, i + 1));
 }
