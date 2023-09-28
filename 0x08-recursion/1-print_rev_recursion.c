802452e335f6125640402c232b01b1ac738b6ac9
weamosama <weam.hassanein@gmail.com>
2023-09-28 08:53:49 -0700

    ggg

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_puts_rev_recursion - chr
+ * @s: fff
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	_putchar('\n');
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,4 +2,5 @@
 #define _MAIN_H_
 void _puts_recursion(char *s);
 int _putchar(char c);
+void _print_rev_recursion(char *s);
 #endif
