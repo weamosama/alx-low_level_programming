2ba752d2cfb7cba69325e79578363fde17eb2598
weamosama <weam.hassanein@gmail.com>
2023-10-02 14:19:15 -0700

    gg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *@argc:fff
+ *@argv: arra
+ *Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
