7b9f32ccee06eb506e8d014e545a82692046e47d
weamosama <weam.hassanein@gmail.com>
2023-10-02 14:04:33 -0700

    gg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *@argc:fff
+ *@argv: arra
+ *Return: int
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+	return 0;
+}
