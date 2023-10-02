78fe1505d321ef0cfe8d0944076fc4651b4d3cf6
weamosama <weam.hassanein@gmail.com>
2023-10-02 14:31:18 -0700

    gg

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,13 +8,13 @@
  */
 int main(int argc, char *argv[])
 {
+	int num1, num2, result;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int num1, num2, result;
-
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 	result = num1 * num2;
