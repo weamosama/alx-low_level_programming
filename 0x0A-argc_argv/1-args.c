57cea8d1e506585a04cce5bf57970d890d1276c6
weamosama <weam.hassanein@gmail.com>
2023-10-02 14:07:26 -0700

    gg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *@argc:fff
+ *@argv: arra
+ *Return: int
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%d\n", argc - 1);
+	}
+
+	return (0);
+}
