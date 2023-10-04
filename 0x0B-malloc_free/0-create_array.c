624693c124c5d4d54146b67691e7169b997400c8
weamosama <weam.hassanein@gmail.com>
2023-10-04 12:06:45 -0700

    test

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - test
+ * @size: dddd
+ * @c: fff
+ * Return: test
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *arr;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	arr = (char*) malloc(size* sizeof(char));
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+
+	return (arr);
+}
