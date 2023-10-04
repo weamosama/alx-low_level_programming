53b78a9d0bbd94f69e6cb6644bc8384ad305c2ea
weamosama <weam.hassanein@gmail.com>
2023-10-04 13:47:04 -0700

    test

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,19 +9,22 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+	char *arr;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
 
-	char *arr = (char*) malloc(size* sizeof(char));
+	arr = (char*) malloc(size* sizeof(char));
 
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
 
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
