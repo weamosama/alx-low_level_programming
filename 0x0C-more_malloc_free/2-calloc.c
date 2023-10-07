98fc79189c7613025deb67a20605fd7e099807bd
weamosama <weam.hassanein@gmail.com>
2023-10-07 14:38:21 -0700

    sss

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_calloc - Allocate memory for an array and set it to zero
+ *@nmemb: Number of elements in the array
+ *@size: Size of each element in bytes
+ *
+ *Return: A pointer to the allocated memor
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *pointr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	pointr = malloc(nmemb * size);
+
+	if (pointr == NULL)
+		return (NULL);
+
+	for (unsigned int i = 0; i < nmemb * size; i++)
+		*((char *) pointr + i) = 0;
+
+	return (pointr);
+
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,4 +3,5 @@
 
 
 void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
 #endif
