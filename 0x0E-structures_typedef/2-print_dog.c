cb694f1df4a966b6e04d57344a02d8d352ee53bf
weamosama <weam.hassanein@gmail.com>
2023-10-10 09:49:13 -0700

    m

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,6 +12,6 @@ void print_dog(struct dog *d)
 	}
 
 	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-	printf("Age: %.2f\n", d->age);
+	printf("Age: %d\n", d->age);
 	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
