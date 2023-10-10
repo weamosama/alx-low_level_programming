50034c1a5174b712cc7626c90723203933e30be9
weamosama <weam.hassanein@gmail.com>
2023-10-10 08:59:31 -0700

    m

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ * Description: This is where the program execution begins.
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
