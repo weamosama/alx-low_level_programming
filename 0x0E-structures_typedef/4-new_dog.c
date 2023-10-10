31c2a64825e33fd00e88fcec182f7ee42d70cab9
weamosama <weam.hassanein@gmail.com>
2023-10-10 11:16:32 -0700

    m

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,37 +1,35 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-/**
- *new_dog - Creates a new dog_t and stores a copy of name and owner.
- *@name: Pointer to a string containing the dog's name.
- *@age: The age of the dog.
- *@owner: Pointer to a string containing the owner's nam
- *Return: Pointer
- */
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t *new_dog_ptr = malloc(sizeof(dog_t));
 
-	if (new_dog_ptr == NULL)
-	{
+/**
+ *new_dog - entry point
+ *@name: string from main, name of pet
+ *@age: number from main, age of pet
+ *@owner: string from main, owner of pet
+ *Return: pointer
+ */
+dog_t* new_dog(char *name, float age, char *owner)
+{
+	dog_t * p;
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
 		return (NULL);
-	}
 	if (name == NULL)
 	{
+		free(p);
 		free(owner);
-		free(new_dog_ptr);
 		return (NULL);
 	}
+
 	if (owner == NULL)
 	{
+		free(p);
 		free(name);
-		free(new_dog_ptr);
 		return (NULL);
 	}
-	new_dog_ptr->name = name;
-	new_dog_ptr->owner = owner;
-	new_dog_ptr->age = age;
 
-	return (new_dog_ptr);
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
 }
