67f78be16f10a79fb77f7446609f1dc48f7b257b
weamosama <weam.hassanein@gmail.com>
2023-10-10 11:18:23 -0700

    m

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
-
+#include <string.h>
 /**
  *new_dog - entry point
  *@name: string from main, name of pet
@@ -8,28 +8,31 @@
  *@owner: string from main, owner of pet
  *Return: pointer
  */
-dog_t* new_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t * p;
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
+	int len_name, len_owner;
+	struct dog *new_dog;
+
+	new_dog = malloc(sizeof(struct dog));
+	if (new_dog == NULL)
 		return (NULL);
-	if (name == NULL)
+	len_name = strlen(name);
+	new_dog->name = malloc(sizeof(char) * (len_name + 1));
+	if (new_dog->name == NULL)
 	{
-		free(p);
-		free(owner);
+		free(new_dog);
 		return (NULL);
 	}
-
-	if (owner == NULL)
+	new_dog->name = strcpy(new_dog->name, name);
+	new_dog->age = age;
+	len_owner = strlen(owner);
+	new_dog->owner = malloc(sizeof(char) * (len_owner + 1));
+	if (new_dog->owner == NULL)
 	{
-		free(p);
-		free(name);
+		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
 	}
-
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	return (p);
+	new_dog->owner = strcpy(new_dog->owner, owner);
+	return (new_dog);
 }
