1ee4d03e569932b3156f25b393dd4d2fb873984b
weamosama <weam.hassanein@gmail.com>
2023-10-10 09:57:29 -0700

    m

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,12 +10,13 @@
  *@owner: Pointer to a string containing the owner's nam
  *Return: Pointer
  */
-dog_t* new_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog_ptr = malloc(sizeof(dog_t));
+
 	if (new_dog_ptr == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	new_dog_ptr->name = strdup(name);
@@ -26,10 +27,10 @@ dog_t* new_dog(char *name, float age, char *owner)
 		free(new_dog_ptr->name);
 		free(new_dog_ptr->owner);
 		free(new_dog_ptr);
-		return NULL;
+		return (NULL);
 	}
 
 	new_dog_ptr->age = age;
 
-	return new_dog_ptr;
+	return (new_dog_ptr);
 }
