dd71f88f28c08b90a195ab6078118c070ab2baf8
weamosama <weam.hassanein@gmail.com>
2023-10-10 09:32:12 -0700

    m

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - Represents information about a dog.
+ *@name: Pointer to a string containing the dog's name.
+ *@age: The age of the dog.
+ *@owner: i to a string
+ *@owner:  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
