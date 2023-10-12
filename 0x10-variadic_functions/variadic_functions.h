f1ce24fc9aa890e938513dc1ee01cedbfb4a9a47
weamosama <weam.hassanein@gmail.com>
2023-10-12 09:22:56 -0700

    fff

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+void print_strings(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
