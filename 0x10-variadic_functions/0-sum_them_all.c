136bf40916d02b22d529322eeba58f6fa6d77ef4
weamosama <weam.hassanein@gmail.com>
2023-10-12 09:06:58 -0700

    fff

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,7 +13,7 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 
 	if (n == 0)
-		return 0;
+		return (0);
 
 	va_start(args, n);
 
@@ -24,5 +24,5 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_end(args);
 
-	return sum;
+	return (sum);
 }
