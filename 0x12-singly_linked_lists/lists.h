87cf374a1e8b21d5e17ac012228ad5474d60db43
weamosama <weam.hassanein@gmail.com>
2023-10-19 10:53:38 -0700

    h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,12 +1,18 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stddef.h>
-
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to
+ * Description: singly linked
+ */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct list_s * next;
+	struct list_s *next;
 }
 
 list_t;
