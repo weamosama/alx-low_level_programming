833e0264bc746017dc047eb57f1c3f557187a997
weamosama <weam.hassanein@gmail.com>
2023-10-24 14:00:53 -0700

    f

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ *listint_len - returns the number of elements
+ *@h: pointer to the head
+ *Return: the number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+
+	count = 0
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
