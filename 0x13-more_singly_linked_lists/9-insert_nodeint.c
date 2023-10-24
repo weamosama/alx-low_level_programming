2e743eebc9890c2cab5571e6c004c413f2ea2ea2
weamosama <weam.hassanein@gmail.com>
2023-10-24 14:46:28 -0700

    f

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,7 +7,7 @@
  *@n: the integer value to be stored
  *Return: the address of the new node, or NULL if it failed
  */
-listint_t* insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node, *current;
 	unsigned int i;
