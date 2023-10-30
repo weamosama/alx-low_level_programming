fda6568171555a7ea5445f3d27c2c39dea903420
weamosama <weam.hassanein@gmail.com>
2023-10-30 13:15:56 -0700

    f

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,7 +2,7 @@
 
 /**
  *print_error_close - print file close error and exit.
- *@file_des: file descriptor
+ *@file_des: file
  */
 void print_error_close(int file_des)
 {
@@ -12,7 +12,7 @@ void print_error_close(int file_des)
 
 /**
  *print_error_read - print file read error and exit.
- *@file_name: file name
+ *@file_name: file
  */
 void print_error_read(char *file_name)
 {
@@ -22,7 +22,7 @@ void print_error_read(char *file_name)
 
 /**
  *print_error_write - print file write error and exit.
- *@file_name: file name
+ *@file_name: file
  */
 void print_error_write(char *file_name)
 {
@@ -46,20 +46,17 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fp_from = open(argv[1], O_RDONLY);
 	if (argv[1] == NULL || fp_from < 0)
 	{
 		print_error_read(argv[1]);
 	}
-
 	fp_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fp_to < 0)
 	{
 		print_error_write(argv[2]);
 		close(fp_from);
 	}
-
 	while ((wc = read(fp_from, buffer, 1024)) > 0)
 	{
 		if (wc != write(fp_to, buffer, wc))
@@ -69,21 +66,16 @@ int main(int argc, char *argv[])
 			close(fp_to);
 		}
 	}
-
 	if (wc < 0)
 	{
 		print_error_read(argv[1]);
 	}
-
 	if (close(fp_from) < 0)
 	{
 		print_error_close(fp_from);
 	}
-
 	if (close(fp_to) < 0)
 	{
-		print_error_close(fp_to);
-	}
-
+		print_error_close(fp_to); }
 	return (0);
 }
