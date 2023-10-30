97153ce465b6a80ec2252823ec1493c7ebc5d7ec
weamosama <weam.hassanein@gmail.com>
2023-10-30 12:17:16 -0700

    f

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,31 +5,31 @@
 
 /**
  *read_textfile - Reads and prints
- *@filename: The name of the file
- *@letters: The number of letters.
+ *@filename: The name of the file to read.
+ *@letters: The number of letters to read and print
  *Return: The actual number of letters read and printed. Returns 0 on error.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	if (filename == NULL)
-	{
 		return (0);
-	}
 
-	int file_descriptor = open(filename, O_RDONLY);
+	int file_descriptor;
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
+
+	file_descriptor = open(filename, O_RDONLY);
 	if (file_descriptor == -1)
-	{
 		return (0);
-	}
 
-	char *buffer = malloc(letters);
+	buffer = malloc(letters);
 	if (buffer == NULL)
 	{
 		close(file_descriptor);
 		return (0);
 	}
 
-	ssize_t bytes_read = read(file_descriptor, buffer, letters);
+	bytes_read = read(file_descriptor, buffer, letters);
 	if (bytes_read == -1)
 	{
 		close(file_descriptor);
@@ -37,7 +37,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
 		close(file_descriptor);
