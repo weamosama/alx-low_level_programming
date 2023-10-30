6f492378ad50784bc9bbe73d1bc93595ca2564fa
weamosama <weam.hassanein@gmail.com>
2023-10-30 12:28:28 -0700

    f

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *create_file - Creates a file
+ *create_file - Creates a file.
  *@filename: The name of the file to create.
- *@text_content: A NULL-terminated string to write to the file.
+ *@text_content: A NULL-terminated
  *Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
