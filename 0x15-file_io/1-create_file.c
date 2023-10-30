f8b56c23ad5a33f4e2517c374a5295df5ab3a9c4
weamosama <weam.hassanein@gmail.com>
2023-10-30 12:39:47 -0700

    f

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,29 +11,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	unsigned int file_descriptor;
+	int fd;
+	ssize_t t_length;
+	ssize_t written;
+	if (!filename)
+		return (-1);
 
-	if (filename == NULL)
-		return -1;
-
-	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (file_descriptor == -1)
-		return -1;
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
 
 	if (text_content != NULL)
 	{
-		ssize_t text_length = 0;
-		while (text_content[text_length])
-			text_length++;
+		t_length = 0;
+		while (text_content[t_length])
+			t_length++;
 
-		ssize_t bytes_written = write(file_descriptor, text_content, text_length);
-		if (bytes_written != text_length)
+		written = write(fd, text_content, t_length);
+		if (written != t_length)
 		{
-			close(file_descriptor);
-			return -1;
+			close(fd);
+			return (-1);
 		}
 	}
 
-	close(file_descriptor);
-	return 1;
+	close(fd);
+	return (1);
 }
