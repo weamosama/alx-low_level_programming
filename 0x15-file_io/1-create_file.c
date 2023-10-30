933948050665e3ec858ebb47ee7084649922167e
weamosama <weam.hassanein@gmail.com>
2023-10-30 12:23:47 -0700

    f

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *create_file - Creates a file
+ *@filename: The name of the file to create.
+ *@text_content: A NULL-terminated string to write to the file.
+ *Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor;
+
+	if (filename == NULL)
+		return -1;
+
+	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file_descriptor == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		ssize_t bytes_written = write(file_descriptor, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
+
+	close(file_descriptor);
+	return (1);
+}
